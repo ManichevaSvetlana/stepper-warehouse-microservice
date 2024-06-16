896a3bc6105df7afdca3d8850d254aa3127132dd
svitlana <manichevassvetlana@gmail.com>
2024-06-16 19:17:36 +0400

    feature: added commands runner

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('shop:update-shop-data --section=features')->weeklyOn(1, '22:00');
 
 Schedule::command('poizon:update-poizon-shop-data')->dailyAt('15:00');
-Schedule::command('sync:systems-products --system=poizon-shop --section=shop --count=10000')->dailyAt('02:00');
+Schedule::command('sync:systems-products --system=poizon-shop --section=shop --count=10000 --images=0')->dailyAt('02:00');
