12f9f8f506df4dadb8e797ab5fe1815d47dc3e1f
svitlana <manichevassvetlana@gmail.com>
2024-08-04 23:16:17 +0400

    test: seeder

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('shop:update-shop-data --section=features')->weeklyOn(1, '22:00');
 
-Schedule::command('poizon:update-poizon-shop-data')->dailyAt('13:00');
+Schedule::command('poizon:update-poizon-shop-data')->dailyAt('09:00');
 Schedule::command('sync:systems-products --system=poizon-shop --section=shop --count=10000 --images=0')->dailyAt('02:00');
